219bcea5c098514d711260618f7f3f44b730aa74
Nima Shoghi <nimashoghi@gmail.com>
2018-08-02 18:14:12 -0400

    Basic project setup

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const presets = [
+    [
+        "@babel/env",
+        {
+            targets: { node: "6.10" },
+            useBuiltIns: "usage"
+        }
+    ]
+];
+
+module.exports = { presets };
